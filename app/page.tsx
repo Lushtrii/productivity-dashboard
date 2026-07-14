564c2aaef8a69f5606b4533768b24dd286748f59
Sean Ford <seanford@protonmail.com>
2026-07-14 09:52:30 -0700

    feat: add basic habit tracker container ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import HabitSummary from "@/components/habit-summary";
+import HabitTracker from "@/components/habit-tracker";
 
 export default function Home() {
   return (
     <main className="w-full bg-white dark:bg-black sm:items-start">
-      <HabitSummary />
+      <HabitTracker />
     </main>
   );
 }
--- components/habit-tracker.tsx
+++ components/habit-tracker.tsx
@@ -1,3 +1,23 @@
+import HabitSummary from "@/components/habit-summary";
+
 export default function HabitTracker() {
-  return <div>Hello world</div>;
+  const sampleTitles = [
+    "Sleep for 8 hours",
+    "Get 10,000 steps",
+    "Code for 2 hours",
+    "No soda",
+  ];
+  const sampleIds = [
+    "019f6184-a2c1-7b3c-94d0-55c1f22c378e",
+    "019f6184-a2c1-7f39-815c-b89c09c4a7d8",
+    "019f6184-a2c1-733b-973f-e02ddd0dcafa",
+    "019f6184-a2c1-7dc7-9ac0-0f05a75747b5",
+  ];
+  return (
+    <div className="p-2 flex flex-col gap-4 border-3 rounded-2xl w-fit">
+      {sampleTitles.map((title, i) => (
+        <HabitSummary title={title} key={sampleIds[i]} />
+      ))}
+    </div>
+  );
 }
