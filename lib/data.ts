daf76780f66b6ab839985e93e04034a162ad94a4
Sean Ford <seanford@protonmail.com>
2026-07-13 17:41:43 -0700

    feat: query active block sessions (#8)

--- lib/data.ts
+++ lib/data.ts
@@ -21,6 +21,17 @@ interface HabitCompletion {
   targetDate: Temporal.PlainDate;
 }
 
+interface ActiveBlockSessionSummary {
+  id: string;
+  title: string;
+  activeTimes: TimeRange[];
+}
+
+interface TimeRange {
+  startTime: Temporal.PlainTime;
+  endTime: Temporal.PlainTime;
+}
+
 export async function getAllTodos(): Promise<Todo[]> {
   const todos = await sql<Todo[]>`SELECT * FROM todo_item`;
   return todos;
@@ -52,4 +63,42 @@ export async function getLastSevenDaysHabitResults(): Promise<HabitResult[]> {
   }));
 
   return results;
+}
+
+function convertTimesToTimeRanges(times: string[]): TimeRange[] {
+  const result: TimeRange[] = [];
+  for (let i = 0; i < times.length; i += 2) {
+    const startTime = Temporal.PlainTime.from(times[i]);
+    const endTime =
+      times[i + 1] === "24:00:00"
+        ? new Temporal.PlainTime(23, 59, 59)
+        : Temporal.PlainTime.from(times[i + 1]);
+    result.push({
+      startTime,
+      endTime,
+    });
+  }
+  return result;
+}
+
+export async function getActiveBlockSessions(): Promise<
+  ActiveBlockSessionSummary[]
+> {
+  const now = Temporal.Now.plainDateTimeISO();
+  // We store day of week schedule as a single number where the first seven bits represent days of the week
+  const dayBit = now.dayOfWeek === 7 ? 1 : 1 << now.dayOfWeek;
+  const result = await sql`
+  SELECT 
+    id, 
+    title,
+    STRING_TO_ARRAY(TRANSLATE(active_times::text, '{}[]()', ''), ',') as active_times
+  FROM block_session 
+  WHERE (active_days_of_week & ${dayBit}) > 0 
+    AND (active_times @> ${now.toPlainTime().toString()}::time)
+`;
+  return result.map((session) => ({
+    id: session.id,
+    title: session.title,
+    activeTimes: convertTimesToTimeRanges(session.activeTimes),
+  }));
 }
